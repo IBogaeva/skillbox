0675f46fd72c8370eb3dcf4cab3fbd41ebd5330d
IBogaeva <ibogaeva@sportmaster.ru>
2021-02-01 14:21:55 +0300

    add pagination

--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-import data from './data';
-import api from './api';
-
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
-
-const msg = data.message + data.text;
-alert(msg);
-api.action();
