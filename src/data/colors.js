de3bdf8f1de3234341df581b64b56ca2474b993a
IBogaeva <ibogaeva@sportmaster.ru>
2021-02-04 16:09:38 +0300

    add product filter

---
+++ src/data/colors.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    colorCode: '#73B6EA',
+    backgroundColor: 'background-color: #73B6EA;',
+  },
+  {
+    id: 2,
+    colorCode: '#FFBE15',
+    backgroundColor: 'background-color: #FFBE15;',
+  },
+  {
+    id: 3,
+    colorCode: '#939393',
+    backgroundColor: 'background-color: #939393;',
+  },
+  {
+    id: 4,
+    colorCode: '#8BE000',
+    backgroundColor: 'background-color: #8BE000;',
+  },
+  {
+    id: 5,
+    colorCode: '#FF6B00',
+    backgroundColor: 'background-color: #FF6B00;',
+  },
+  {
+    id: 6,
+    colorCode: '#FFF',
+    backgroundColor: 'background-color: #FFF;',
+  },
+  {
+    id: 7,
+    colorCode: '#000',
+    backgroundColor: 'background-color: #000;',
+  },
+];
